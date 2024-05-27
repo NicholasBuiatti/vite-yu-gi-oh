ca7c419c0f49e9f41cb4a193f4da8ce8d266f855
NicholasBuiatti <nic.buiatti@gmail.com>
2024-05-27 16:27:16 +0200

    creazione AppHeader

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
-<script setup>
+<script>
+import AppHeader from "./components/AppHeader.vue";
 
+export default {
+    components: {
+        AppHeader
+    },
+    data() {
+        return {
+
+        }
+    }
+}
 </script>
 
 <template>
-
+    <section class="container">
+        <AppHeader />
+    </section>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+    <div class="row bg-white">
+        <figure class="col-2 mb-0">
+            <img src="../assets/Logo.jpg" alt="" class="col-10">
+        </figure>
+        <h1 class="col-3">Yu-Gi-Oh Api</h1>
+    </div>
+</template>
+
+<style scoped></style>
